9c669155fbb3f5d0c5a19050ed0b8824a41666e9
light94 <mishra.rahul1712@gmail.com>
2015-11-05 02:29:25 +0530

    Added tray icon.
    The tray icon has menu to exit the application.
    Image is currently missing.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-var nw = require('nw.gui');
+var gui = require('nw.gui');
+
+// get current window
+
+var win = gui.Window.get();
+
+
+// add icon to tray
+var tray = new gui.Tray({title:'Chatty',icon:'img/icon.png'})
+
+
+// add menu
+var menu = new gui.Menu();
+
+// add exit menuitem to tray
+menu.append(new gui.MenuItem(
+			{
+				label:'Exit',
+				click: function()
+				{
+					win.close();
+				}
+			}));
+tray.menu = menu;
+
+
