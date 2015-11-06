37472dc4d5156b19cc8ad1b18166f83b2523da6d
light94 <mishra.rahul1712@gmail.com>
2015-11-06 09:57:39 +0530

    Tray icon added
    1) Tray icon added( currently image missing)
    2) Hides when closed. Closed when "exit" from tray menu is clicked.

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,42 @@ var win = gui.Window.get();
 
 
 // add icon to tray
-var tray = new gui.Tray({title:'Chatty',icon:'img/icon.png'})
+var tray = new gui.Tray(
+			{
+			title:'Chatty',
+			icon:'img/icon.png',
+			click: function()
+			{
+				win.show();
+			}
+			})
 
 
 // add menu
 var menu = new gui.Menu();
 
 // add exit menuitem to tray
+// exit flag will determine if window needs to be hidden or reduced to tray, like skype
+var exit = false;
 menu.append(new gui.MenuItem(
 			{
 				label:'Exit',
 				click: function()
 				{
+					exit = true;
 					win.close();
-				}
+					
+				},
+				
 			}));
 tray.menu = menu;
 
+// close window if exit pressed , else hide
+win.on('close',function()
+{
+	if(exit)
+		this.close(true);// true (force) is important here, else it will be an infinite loop
+	else
+		this.hide();
+});
 
