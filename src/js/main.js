28826219c0cdcb86a627fda56c91907b4dc61e92
light94 <mishra.rahul1712@gmail.com>
2015-11-07 22:01:39 +0530

    Refactored code

--- src/js/main.js
+++ src/js/main.js
@@ -6,15 +6,13 @@ var win = gui.Window.get();
 
 
 // add icon to tray
-var tray = new gui.Tray(
-			{
-			title:'Chatty',
-			icon:'src/images/chat_icon.png',
-			click: function()
-			{
-				win.show();
-			}
-			})
+var tray = new gui.Tray({
+    title: 'Chatty',
+    icon: 'src/images/chat_icon.png',
+    click: function() {
+        win.show();
+    }
+})
 
 
 // add menu
@@ -23,25 +21,21 @@ var menu = new gui.Menu();
 // add exit menuitem to tray
 // exit flag will determine if window needs to be hidden or reduced to tray, like skype
 var exit = false;
-menu.append(new gui.MenuItem(
-			{
-				label:'Exit',
-				click: function()
-				{
-					exit = true;
-					win.close();
-					
-				},
-				
-			}));
+menu.append(new gui.MenuItem({
+    label: 'Exit',
+    click: function() {
+        exit = true;
+        win.close();
+
+    },
+
+}));
 tray.menu = menu;
 
 // close window if exit pressed , else hide
-win.on('close',function()
-{
-	if(exit)
-		this.close(true);// true (force) is important here, else it will be an infinite loop
-	else
-		this.hide();
+win.on('close', function() {
+    if (exit)
+        this.close(true); // true (force) is important here, else it will be an infinite loop
+    else
+        this.hide();
 });
-
